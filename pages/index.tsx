c3dae941253ed85c7d767d52f8c68f8032ec446e
M-Sieger <msieger1994@gmail.com>
2021-02-16 16:43:53 +0100

    add Greetings

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,15 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-
+import Greeting from "../components/greetings"
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
         <title>nukebox</title>
         <link rel="icon" href="/favicon.ico" />
+        
       </Head>
+      <Greeting name="Moritz" />
 
      
     </div>
