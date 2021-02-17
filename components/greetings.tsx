8d57081abb1f7aab6a081dd9ad8d4a5f997e3bf5
M-Sieger <msieger1994@gmail.com>
2021-02-17 15:27:19 +0100

    run prettier

--- components/greetings.tsx
+++ components/greetings.tsx
@@ -1,13 +1,13 @@
 import styles from "../styles/Greeting.module.css";
 
 type Props = {
-    name:string;
+  name: string;
 };
 
 export default function Greeting(props: Props) {
-    return (
-      <p>
-        Hello, <span className={styles.name}>{props.name}</span>
-      </p>
-    );
-  }
+  return (
+    <p>
+      Hello, <span className={styles.name}>{props.name}</span>
+    </p>
+  );
+}
