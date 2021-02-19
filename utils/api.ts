977838f7cce3d145e6000d6edcb1f71fbb510914
M-Sieger <msieger1994@gmail.com>
2021-02-19 09:45:17 +0100

    fixed typo of api

--- utils/api.ts
+++ utils/api.ts
@@ -11,7 +11,7 @@ export async function getTracks() {
   return tracks;
 }
 
-export async function getTrack(id: string) {
+export async function getTrack(id) {
   const response = await fetch(`/api/tracks/${id}`);
   const track: APITrack = await response.json();
   return track;
