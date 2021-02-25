f0f928deb214fc8b68cc839a553948c9dba282ea
M-Sieger <msieger1994@gmail.com>
2021-02-25 14:22:40 +0100

    Delete track on DELETE request

--- server/db.ts
+++ server/db.ts
@@ -7,13 +7,13 @@ type DB = {
 
 const DB_PATH = "./server/db.json";
 
-export async function readDB(): Promise<DB> {
+export async function readDb(): Promise<DB> {
   const dbJSON = await fs.readFile(DB_PATH, "utf-8");
   const db: DB = JSON.parse(dbJSON);
   return db;
 }
 
-export async function writeDB(db: DB) {
-  const dbJSON = JSON.stringify(db);
-  await fs.writeFile("./server/db.json", dbJSON);
+export async function writeDb(db: DB) {
+  const dbJSON = JSON.stringify(db, null, 2);
+  await fs.writeFile(DB_PATH, dbJSON);
 }
